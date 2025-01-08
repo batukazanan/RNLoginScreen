1bfb1da3b2d2d1994d08ebf87c26b56ab9803197
Batuhan Kazanan <batukazanan@gmail.com>
2025-01-08 17:35:08 +0300

    custom text input created

---
+++ src/components/CustomTextInput.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+
+const CustomTextInput = ({title, placeholder, isSecureText, onChangeText, value}) => {
+    return (
+        <View style={styles.inputContainer}>
+            <Text style={styles.inputBoxText} >{title}</Text>
+            <TextInput
+                placeholder={placeholder}
+                secureTextEntry={isSecureText}
+                style={styles.TextInputStyle}
+                onChangeText={onChangeText}
+                value={value}
+            />
+        </View>
+    )
+}
+
+export default CustomTextInput
+
+const styles = StyleSheet.create({
+
+    inputContainer: {
+        width: '80%',
+    },
+
+    inputBoxText: {
+        fontWeight: 'bold',
+    },
+
+    TextInputStyle: {
+        borderWidth: 0.2,
+        borderRadius: 7,
+        width: '100%',
+        height: 50,
+        marginVertical: 12,
+        textAlign: 'center',
+        backgroundColor: '#F7F8F9'
+    }
+
+})
