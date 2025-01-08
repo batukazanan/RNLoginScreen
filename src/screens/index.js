5203f733c224e00c2ab56fa29b60054044bf3a5b
Batuhan Kazanan <batukazanan@gmail.com>
2025-01-08 10:02:10 +0300

    some imports added

---
+++ src/screens/index.js
@@ -0,0 +1,13 @@
+import LoginPage from "./LoginPage";
+import HomePage from "./HomePage";
+import SignupPage from "./SignupPage";
+import ProfilePage from "./ProfilePage";
+
+
+
+export {
+    ProfilePage,
+    LoginPage,
+    HomePage,
+    SignupPage
+}
