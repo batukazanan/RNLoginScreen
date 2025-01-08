7a108bc1cc36a060e6dfab1af0f69ff13deefeb2
Batuhan Kazanan <batukazanan@gmail.com>
2025-01-08 17:36:34 +0300

    Implemented custom button component, communicated with props and cleaned unnecessary files

--- src/screens/EntryPage.js
+++ src/screens/EntryPage.js
@@ -1,4 +1,5 @@
-import { Pressable, StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
+import { CustomButton } from '../components'
 import React from 'react'
 
 const EntryPage = ({ navigation }) => {
@@ -14,21 +15,23 @@ const EntryPage = ({ navigation }) => {
 
             <View style={styles.buttonsContainer}>
 
-                <Pressable
-                    onPress={() => navigation.navigate('Login')}
-                    style={styles.loginButton}>
-                    <Text style={styles.loginText}>Login</Text>
-                </Pressable>
+                <CustomButton
+                    buttonText='Login'
+                    handleOnPress={() => navigation.navigate('Login')}
+                    buttonColor={'black'}
+                    buttonTextColor={'white'}
+                />
 
-                <Pressable
-                    onPress={() => navigation.navigate('SignUp')}
-                    style={styles.signupButton}>
-                    <Text style={styles.signupText}>Sign Up</Text>
-                </Pressable>
+                <CustomButton
+                    buttonText='Sign Up'
+                    handleOnPress={() => navigation.navigate('SignUp')}
+                    buttonColor={'white'}
+                    buttonTextColor={'black'}
+
+                />
+
             </View>
-
         </View>
-
     )
 }
 
@@ -50,38 +53,5 @@ const styles = StyleSheet.create({
     imageContainer: {
         flex: 2,
     },
-
-    signupButton: {
-        width: "70%",
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 8,
-        marginTop: 20,
-        borderWidth: 1.7,
-        backgroundColor: 'white'
-    },
-
-    signupText: {
-        fontSize: 15,
-        fontWeight: '500'
-    },
-
-    loginButton: {
-        width: "70%",
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 8,
-        marginTop: 20,
-        borderWidth: 1,
-        backgroundColor: 'black'
-    },
-
-    loginText: {
-        fontSize: 15,
-        fontWeight: '500',
-        color: 'white'
-    }
 
 })
